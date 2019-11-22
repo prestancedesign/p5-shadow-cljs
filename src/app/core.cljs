b718d0ac0d7a13ba21351fb74562232ecba534d9
Michael SALIHI <admin@prestance-informatique.fr>
2019-11-22 18:36:41 +0100

    Simple example from p5.js "get started" page in CLJS

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,11 +1,19 @@
-(ns app.core)
+(ns app.core
+  (:require [goog.object :as g]))
 
-(defn ^:export setup []
-  (js/createCanvas 640 480))
+(defn setup []
+  (let [cnv (js/createCanvas 640 480)
+        x (/ (- (.-windowWidth js/window) (.-width js/window)) 2)
+        y (/ (- (.-windowHeight js/window) (.-height js/window)) 2)]
+    (.position cnv x y)
+    (js/background 235)))
 
-(defn ^:export draw
-  []
+(defn draw []
   (if js/mouseIsPressed
     (js/fill 255)
     (js/fill 155))
   (js/ellipse js/mouseX js/mouseY 80 80))
+
+(doto js/window
+  (g/set "setup" setup)
+  (g/set "draw" draw))
